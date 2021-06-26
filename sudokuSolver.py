423c776b369ab1e04666861233133910286de81c
Justin Wang <64336791+justinwang8@users.noreply.github.com>
2021-06-26 15:52:51 -0400

    3:52PM

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -3,31 +3,34 @@ import cv2 as cv
 from matplotlib import pyplot as plt
 import numpy as np
 
+img = cv.imread('Sudoku/sudoku.png')
+h, w, c = img.shape
 
-initialWidth = 66
 
-for i in range(9):
+initialWidth = w/18
+widthIncrement = w/9
+initialHeight = h/18
+heightIncrement = h/9
+reader = easyocr.Reader(['en'], gpu=False)
+for j in range(9):
+    for i in range(9):
 
-    img = cv.imread('Sudoku/sudoku.png')
+        blank = np.zeros(img.shape[:2], dtype='uint8')
 
-    blank = np.zeros(img.shape[:2], dtype='uint8')
+        mask = cv.circle(
+            blank, (int(initialWidth), int(initialHeight)), 66, 255, -1)
 
-    mask = cv.circle(
-        blank, (initialWidth, 198), 66, 255, -1)
+        masked = cv.bitwise_and(img, img, mask=mask)
+        #cv.imshow('Masked Image', masked)
 
-    masked = cv.bitwise_and(img, img, mask=mask)
-    cv.imshow('Masked Image', masked)
+        result = reader.readtext(masked)
 
-    reader = easyocr.Reader(['en'], gpu=False)
+        print(result)
 
-    result = reader.readtext(masked)
+        initialWidth += widthIncrement
 
-    print(result)
-
-    initialWidth += 133
-
-    print(i)
+        print(i)
+    initialWidth = w/18
+    initialHeight += heightIncrement
 
 cv.waitKey(0)
-
-
